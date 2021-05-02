33a5d9d6320c457bbdc0c5043c40aa0ae4112b98
Tanay Kapoor <itstkaay@gmail.com>
2021-05-02 13:05:07 +0530

    Changes in some files

--- com/personalnotes/elseif.java
+++ com/personalnotes/elseif.java
@@ -1,2 +1,13 @@
-package com.personalnotes;public class elseif {
-}
+//package com.personalnotes;
+//
+//public class elseif {
+//    public static void main(String[] args) {
+//        if (condition) {
+////            Statements;
+//        } else if (condition) {
+////            Statements;
+//        } else {
+////            Statements;
+//        }
+//    }
+//}
